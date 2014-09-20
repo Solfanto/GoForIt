f3f2a2571497ce130d0c7e3110b089c09cd1630d
安部 裕介 <yusuke@yusuke-2.local>
2014-09-20 18:22:50 +0900

    added alarms

--- GoForIt/AppDelegate.swift
+++ GoForIt/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let navigationController = UINavigationController(rootViewController: AlarmViewController(style: .Plain))
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 
