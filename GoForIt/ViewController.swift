711abeeef8b06a87c283b2f5e80cb26734ec6730
n-studio <contact@n-studio.fr>
2014-09-20 11:03:24 +0900

    setup XCode project

---
+++ GoForIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoForIt
+//
+//  Created by Lidner on 14/9/20.
+//  Copyright (c) 2014 Solfanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
